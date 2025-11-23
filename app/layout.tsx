83174760457e9589bcf4e58cd05a1d8564c02bb6
Gyu <pptn2@kakao.com>
2025-11-24 00:09:51 +0900

    feat: Add Google Analytics (gtag.js) tracking script to the head.

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,22 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DFE53QVVXN"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-DFE53QVVXN');
+            `,
+          }}
+        />
         {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
