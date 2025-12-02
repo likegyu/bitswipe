39c0ca76be73bdeba5a56d5d86aa9f0d52ce6537
likegyu <gyu@likegyu.com>
2025-12-02 14:55:04 +0900

    robots.ts 업데이트

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://bitswipe.xyz/sitemap.xml',
+        sitemap: 'https://www.bitswipe.xyz/sitemap.xml',
     }
 }
